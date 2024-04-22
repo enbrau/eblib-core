72f6e1253ba7f3ae53179f36334c98dbe2aee40b
Enbrau <enbrau@gmail.com>
2024-04-22 19:23:05 +0800

    chore: add build formats

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,43 @@
 export default [
   {
     input: './src/index.js',
-    output: {
-      dir: 'dist',
-      format: 'cjs',
-      entryFileNames: 'index.cjs.js',
-      sourcemap: true
-    }
-  },
-  {
-    input: './src/index.js',
-    output: {
-      dir: 'dist',
-      format: 'esm',
-      entryFileNames: 'index.esm.js',
-      sourcemap: true
-    }
+    output: [
+      {
+        dir: 'dist',
+        format: 'amd',
+        entryFileNames: 'index.amd.js',
+        sourcemap: true
+      },
+      {
+        dir: 'dist',
+        format: 'esm',
+        entryFileNames: 'index.esm.js',
+        sourcemap: true
+      },
+      {
+        dir: 'dist',
+        format: 'cjs',
+        entryFileNames: 'index.cjs.js',
+        sourcemap: true
+      },
+      {
+        dir: 'dist',
+        format: 'system',
+        entryFileNames: 'index.system.js',
+        sourcemap: true
+      },
+      {
+        file: 'dist/index.umd.js',
+        format: 'umd',
+        name: 'eblib',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.iife.js',
+        format: 'iife',
+        name: 'eblib',
+        sourcemap: true,
+      }
+    ]
   }
 ]
