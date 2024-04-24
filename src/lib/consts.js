7687150f5b4f434cd3e6f0afb9a05a9bb81b885a
Enbrau <admin@enbrau.com>
2024-04-24 20:36:35 +0800

    chore: add test case

--- src/lib/consts.js
+++ src/lib/consts.js
@@ -1,4 +1,8 @@
 /**
+ * @module consts
+ */
+
+/**
  * RegExp: hex color
  */
 export const REG_HEX_COLOR = new RegExp(/^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/)
--- src/lib/format.js
+++ src/lib/format.js
@@ -28,10 +28,8 @@ export function toAmount(number, prefix, fixed) {
 export function toFileSize(fileSizeByte) {
   let fileSizeMsg = ''
   if (fileSizeByte < 1048576) fileSizeMsg = (fileSizeByte / 1024).toFixed(2) + 'KB'
-  else if (fileSizeByte === 1048576) fileSizeMsg = '1MB'
-  else if (fileSizeByte > 1048576 && fileSizeByte < 1073741824) fileSizeMsg = (fileSizeByte / (1024 * 1024)).toFixed(2) + 'MB'
-  else if (fileSizeByte > 1048576 && fileSizeByte === 1073741824) fileSizeMsg = '1GB'
-  else if (fileSizeByte > 1073741824 && fileSizeByte < 1099511627776) fileSizeMsg = (fileSizeByte / (1024 * 1024 * 1024)).toFixed(2) + 'GB'
+  else if (fileSizeByte >= 1048576 && fileSizeByte < 1073741824) fileSizeMsg = (fileSizeByte / (1024 * 1024)).toFixed(2) + 'MB'
+  else if (fileSizeByte >= 1073741824 && fileSizeByte < 1099511627776) fileSizeMsg = (fileSizeByte / (1024 * 1024 * 1024)).toFixed(2) + 'GB'
   else fileSizeMsg = '>1TB'
   return fileSizeMsg
 }
