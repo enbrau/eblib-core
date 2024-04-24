10a7c15be01782d9382bf5cc934e084977e44316
Enbrau <enbrau@gmail.com>
2024-04-24 18:24:35 +0800

    feat: add function toFileSize

--- src/lib/format.js
+++ src/lib/format.js
@@ -15,3 +15,19 @@ export function toAmount(number, prefix, fixed) {
   arr[0] = arr[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   return (prefix || '') + arr.join('.')
 }
+
+/**
+ * transform number into file size
+ * @param {Number} fileSizeByte 
+ * @returns file size string
+ */
+export function toFileSize(fileSizeByte) {
+  let fileSizeMsg = ''
+  if (fileSizeByte < 1048576) fileSizeMsg = (fileSizeByte / 1024).toFixed(2) + 'KB'
+  else if (fileSizeByte === 1048576) fileSizeMsg = '1MB'
+  else if (fileSizeByte > 1048576 && fileSizeByte < 1073741824) fileSizeMsg = (fileSizeByte / (1024 * 1024)).toFixed(2) + 'MB'
+  else if (fileSizeByte > 1048576 && fileSizeByte === 1073741824) fileSizeMsg = '1GB'
+  else if (fileSizeByte > 1073741824 && fileSizeByte < 1099511627776) fileSizeMsg = (fileSizeByte / (1024 * 1024 * 1024)).toFixed(2) + 'GB'
+  else fileSizeMsg = '>1TB'
+  return fileSizeMsg
+}
