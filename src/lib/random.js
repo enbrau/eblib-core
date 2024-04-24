5e3787a6d00ae0a96a233ab47d8581d1bf228e41
Enbrau <enbrau@gmail.com>
2024-04-24 10:57:15 +0800

    feat: add function randomString

---
+++ src/lib/random.js
@@ -0,0 +1,11 @@
+/**
+ * generate random string
+ * @param {number} length length of the random string
+ * @returns random string
+ */
+export function randomString(length) {
+  let len = length || 8
+  return len <= 11 ? 
+    Math.random().toString(36).substring(2, 2 + len).padEnd(0) : 
+    randomString(11) + randomString(len - 11)
+}
