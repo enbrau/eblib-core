5883488949e61c52c62a95b144eca7136352c2af
Enbrau <enbrau@gmail.com>
2024-04-24 10:43:09 +0800

    fix: toAmount (F01T0106)

--- src/lib/format.js
+++ src/lib/format.js
@@ -6,7 +6,11 @@
  * @returns amount string
  */
 export function toAmount(number, prefix, fixed) {
-  let num = (typeof number === 'string' ? parseFloat(number) : number).toFixed(fixed === 0 ? 0 : fixed || 2)
+  let num = (typeof number === 'number' ? number : parseFloat(number))
+  if (isNaN(num)) {
+    return ''
+  }
+  num = num.toFixed(fixed === 0 ? 0 : fixed || 2)
   let arr = num.split('.')
   arr[0] = arr[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   return (prefix || '') + arr.join('.')
--- test/lib.test.js
+++ test/lib.test.js
@@ -1,29 +1,35 @@
 import $eblib from '../src/index.js'
 
-describe('C1: color.js', function() {
-  describe('C1T1: rgb and hex transform ', function() {
-    it("C1T1001: rgbToHex('rgb(0,0,0)')", function() {
+describe('C01: color.js', function() {
+  describe('C01T01: rgb and hex transform ', function() {
+    it("C01T0101: rgbToHex('rgb(0,0,0)')", function() {
       expect($eblib.rgbToHex('rgb(0,0,0)')).toBe('#000000')
     })
-    it("C1T1002: hexToRgb('#000000')", function() {
+    it("C01T0102: hexToRgb('#000000')", function() {
       expect($eblib.hexToRgb('#000000')).toBe('rgb(0,0,0)')
     })
   })
 })
 
-describe('F1: format.js', function() {
-  describe('F1T1: amount transform', function() {
-    it("F1T1001: $eblib.toAmount('100000.567')", function() {
+describe('F01: format.js', function() {
+  describe('F01T01: amount transform', function() {
+    it("F01T0101: $eblib.toAmount('100000.567')", function() {
       expect($eblib.toAmount('100000.567')).toBe('100,000.57')
     })
-    it("F1T1002: $eblib.toAmount('100000.567', '$')", function() {
+    it("F01T0102: $eblib.toAmount('100000.567', '$')", function() {
       expect($eblib.toAmount('100000.567', '$')).toBe('$100,000.57')
     })
-    it("F1T1003: $eblib.toAmount('100000.567', '$', 4)", function() {
+    it("F01T0103: $eblib.toAmount('100000.567', '$', 4)", function() {
       expect($eblib.toAmount('100000.567', '$', 4)).toBe('$100,000.5670')
     })
-    it("F1T1004: $eblib.toAmount('100000.567', '$', 0)", function() {
+    it("F01T0104: $eblib.toAmount('100000.567', '$', 0)", function() {
       expect($eblib.toAmount('100000.567', '$', 0)).toBe('$100,001')
+    })
+    it("F01T0105: $eblib.toAmount(100000.567)", function() {
+      expect($eblib.toAmount(100000.567)).toBe('100,000.57')
+    })
+    it("F01T0106: $eblib.toAmount(true)", function() {
+      expect($eblib.toAmount(true)).toBe('')
     })
   })
 })
